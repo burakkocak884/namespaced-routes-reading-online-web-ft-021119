b875e0531e868edad01bf7795bb34822268746c0
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-07 23:49:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  resources :authors, only: %i[show index] do
-    resources :posts, only: %i[show index new edit]
-  end
+  # resources :authors, only: %i[show index] do
+  #   resources :posts, only: %i[show index new edit]
+  # end
 
-  resources :posts, only: %i[index show new create edit update]
+  # resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
 
-  root 'posts#index'
-  #get '/admin/stats', to: 'stats#index'
+  # root 'posts#index'
+  # #get '/admin/stats', to: 'stats#index'
 
 
-   scope '/admin' do 
-   	resources :stats, only: [:index]
-   end
+   namespace :admin do
+  resources :stats, only: [:index]
+end
 end
